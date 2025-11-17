b1fa42d5cf725cc8c34f70436f63e3ddca682229
Lecrut <filip.lecrut12@gmail.com>
2025-11-17 13:59:52 +0100

    random forest

---
+++ scripts/classifiers/random_forests.py
@@ -0,0 +1,17 @@
+#%% Imports
+from sklearn.ensemble import RandomForestClassifier
+
+#%% Random Forest Classifier
+class RFClassifier:
+    def __init__(self, n_estimators=100, max_depth=None):
+        self.model = RandomForestClassifier(
+            n_estimators=n_estimators,
+            max_depth=max_depth
+        )
+
+    def fit(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
+    
