548206871179bb6b1996727d8e7790d801e50f8c
Lecrut <filip.lecrut12@gmail.com>
2025-11-17 14:43:02 +0100

    main - todo

---
+++ main.py
@@ -0,0 +1,20 @@
+#%% Imports
+
+
+#%% Extract dataset paths and labels from a given directory
+
+
+#%% Resize and pad images to a fixed size
+
+
+#%% Use extraction and resizing functions to prepare dataset
+
+
+#%% PCA 
+
+
+#%% Classification 
+
+
+#%% Metrics 
+
