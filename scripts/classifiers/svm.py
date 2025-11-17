3b431cb81ef6633d20ce58a130e1e72b9d73f740
Lecrut <filip.lecrut12@gmail.com>
2025-11-17 13:55:36 +0100

    basic svm

---
+++ scripts/classifiers/svm.py
@@ -0,0 +1,15 @@
+#%% 
+from sklearn.svm import SVC
+from sklearn.multiclass import OneVsRestClassifier
+
+#%% svm class 
+class SVMClassifier:
+    def __init__(self, C=1.0, kernel='rbf', gamma='scale'):
+        base_model = SVC(C=C, kernel=kernel, gamma=gamma)
+        self.model = OneVsRestClassifier(base_model)
+
+    def fit(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
